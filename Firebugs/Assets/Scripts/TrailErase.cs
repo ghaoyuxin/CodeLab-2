54d0806aa617ccba44d779d31a7ecaed4fcc9bc3
ghaoyuxin <ghaoyuxin@gmail.com>
2019-09-14 09:40:09 -0400

    Changed main project folder name thus re-uploaded project

---
+++ Firebugs/Assets/Scripts/TrailErase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailErase : MonoBehaviour
+{
+    private TrailRenderer ink;
+    private int eraseTimeSlow = 100;
+    private float eraseTimeFast = 0.5f;
+    void Start()
+    {
+        ink = GetComponent<TrailRenderer>();
+        ink.time = eraseTimeSlow;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!PublicVars.isDrag)
+        {
+            ink.time = eraseTimeFast;
+        }
+        else if (PublicVars.isDrag)
+        {
+            ink.time = eraseTimeSlow;
+        }
+    }
+}
