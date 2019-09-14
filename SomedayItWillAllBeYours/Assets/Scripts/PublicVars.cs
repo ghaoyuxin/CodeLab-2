797841042a323723f84a5161f619452ed841a9e0
ghaoyuxin <ghaoyuxin@gmail.com>
2019-09-14 09:34:22 -0400

    Init upload existing project "Firebugs"

---
+++ SomedayItWillAllBeYours/Assets/Scripts/PublicVars.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublicVars
+{
+    public static bool isDrag = false;
+}
---
+++ SomedayItWillAllBeYours/Assets/Scripts/StopAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private AudioSource aud;
+
+    private float animSpeed = 1f;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        animSpeed = anim.speed;
+        aud = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PublicVars.isDrag)
+        {
+            anim.speed = 0f;
+            aud.Play();
+            //print("played");
+        }
+        else
+        {
+            anim.speed = animSpeed;
+        }
+    }
+}
