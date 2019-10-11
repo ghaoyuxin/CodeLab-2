deedd2a703b3b60b74694cc586fab26f88c32021
ghaoyuxin <ghaoyuxin@gmail.com>
2019-10-11 02:12:20 -0400

    init commit of w5 hw

---
+++ W5_Gao/Blackjack/Assets/Scripts/BlackJackManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BlackJackManager : MonoBehaviour {
+
+	public Text statusText;
+	public GameObject tryAgain;
+	public string loadScene;
+
+	public void PlayerBusted(){
+		HidePlayerButtons();
+		GameOverText("YOU BUST", Color.red);
+	}
+
+	public void DealerBusted(){
+		GameOverText("DEALER BUSTS!", Color.green);
+	}
+		
+	public void PlayerWin(){
+		GameOverText("YOU WIN!", Color.green);
+	}
+		
+	public void PlayerLose(){
+		GameOverText("YOU LOSE.", Color.red);
+	}
+
+
+	public void BlackJack(){
+		GameOverText("Black Jack!", Color.green);
+		HidePlayerButtons();
+	}
+
+	public void GameOverText(string str, Color color){
+		statusText.text = str;
+		statusText.color = color;
+
+		tryAgain.SetActive(true);
+	}
+
+	public void HidePlayerButtons(){
+		GameObject.Find("HitButton").SetActive(false);
+		GameObject.Find("StayButton").SetActive(false);
+	}
+
+	public void TryAgain(){
+		SceneManager.LoadScene(loadScene);
+	}
+
+	public virtual int GetHandValue(List<DeckOfCards.Card> hand)
+	{
+		int handValue = 0;
+
+		foreach(DeckOfCards.Card handCard in hand){
+			handValue += handCard.GetCardHighValue();
+		}
+		// if player has Black Jack, and the handValue is > 21, the A card value should be 1, 
+		if (!ReferenceEquals(DeckOfCards.Card.Type.A, null) && handValue > 21) return handValue - 10;
+
+		else return handValue;
+	}
+}
