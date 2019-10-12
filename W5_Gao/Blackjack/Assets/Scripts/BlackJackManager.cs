9172b51ec2efe7c2b16ab169d2b3c68065238532
ghaoyuxin <ghaoyuxin@gmail.com>
2019-10-12 18:36:34 -0400

    fixed the bug of Ace is counted as 1 when overall hand value is over 21. The original fix is problematic

--- W5_Gao/Blackjack/Assets/Scripts/BlackJackManager.cs
+++ W5_Gao/Blackjack/Assets/Scripts/BlackJackManager.cs
@@ -4,61 +4,78 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 
-public class BlackJackManager : MonoBehaviour {
+public class BlackJackManager : MonoBehaviour
+{
 
-	public Text statusText;
-	public GameObject tryAgain;
-	public string loadScene;
+    public Text statusText;
+    public GameObject tryAgain;
+    public string loadScene;
 
-	public void PlayerBusted(){
-		HidePlayerButtons();
-		GameOverText("YOU BUST", Color.red);
-	}
+    public void PlayerBusted()
+    {
+        HidePlayerButtons();
+        GameOverText("YOU BUST", Color.red);
+    }
 
-	public void DealerBusted(){
-		GameOverText("DEALER BUSTS!", Color.green);
-	}
-		
-	public void PlayerWin(){
-		GameOverText("YOU WIN!", Color.green);
-	}
-		
-	public void PlayerLose(){
-		GameOverText("YOU LOSE.", Color.red);
-	}
+    public void DealerBusted()
+    {
+        GameOverText("DEALER BUSTS!", Color.green);
+    }
+
+    public void PlayerWin()
+    {
+        GameOverText("YOU WIN!", Color.green);
+    }
+
+    public void PlayerLose()
+    {
+        GameOverText("YOU LOSE.", Color.red);
+    }
 
 
-	public void BlackJack(){
-		GameOverText("Black Jack!", Color.green);
-		HidePlayerButtons();
-	}
+    public void BlackJack()
+    {
+        GameOverText("Black Jack!", Color.green);
+        HidePlayerButtons();
+    }
 
-	public void GameOverText(string str, Color color){
-		statusText.text = str;
-		statusText.color = color;
+    public void GameOverText(string str, Color color)
+    {
+        statusText.text = str;
+        statusText.color = color;
 
-		tryAgain.SetActive(true);
-	}
+        tryAgain.SetActive(true);
+    }
 
-	public void HidePlayerButtons(){
-		GameObject.Find("HitButton").SetActive(false);
-		GameObject.Find("StayButton").SetActive(false);
-	}
+    public void HidePlayerButtons()
+    {
+        GameObject.Find("HitButton").SetActive(false);
+        GameObject.Find("StayButton").SetActive(false);
+    }
 
-	public void TryAgain(){
-		SceneManager.LoadScene(loadScene);
-	}
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(loadScene); // this is a suprising way to loadScene
+    }
 
-	public virtual int GetHandValue(List<DeckOfCards.Card> hand)
-	{
-		int handValue = 0;
+    public virtual int GetHandValue(List<DeckOfCards.Card> hand)//
+    {
+        int handValue = 0;
+        int AceCount = 0;
 
-		foreach(DeckOfCards.Card handCard in hand){
-			handValue += handCard.GetCardHighValue();
-		}
-		// if player has Black Jack, and the handValue is > 21, the A card value should be 1, 
-		if (!ReferenceEquals(DeckOfCards.Card.Type.A, null) && handValue > 21) return handValue - 10;
+        foreach (DeckOfCards.Card handCard in hand)
+        {
+            handValue += handCard.GetCardHighValue();
+            if (handCard.GetCardHighValue() == 11) AceCount++; // ace count
+        }
 
-		else return handValue;
-	}
+        for (int i = 0; i < AceCount; i++)
+        {
+            if (handValue > 21) handValue -= 10;
+        }
+        //if player has Black Jack, and the handValue is > 21, the A card value should be 1, 
+        //if (!ReferenceEquals((DeckOfCards.Card.Type.A), null) && handValue > 21) return handValue - 10;
+
+        return handValue;
+    }
 }
--- W5_Gao/Blackjack/Assets/Scripts/DeckOfCards.cs
+++ W5_Gao/Blackjack/Assets/Scripts/DeckOfCards.cs
@@ -3,117 +3,138 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class DeckOfCards : MonoBehaviour {
-	
-	public Text cardNumUI;
-	public Image cardImageUI;
-	public Sprite[] cardSuits;
+public class DeckOfCards : MonoBehaviour
+{
 
-	public class Card{
+    public Text cardNumUI;
+    public Image cardImageUI;
+    public Sprite[] cardSuits;
 
-		public enum Suit {
-			SPADES, 	//0
-			CLUBS,		//1
-			DIAMONDS,	//2
-			HEARTS	 	//3
-		};
+    public class Card
+    {
 
-		public enum Type {
-			TWO		= 2,
-			THREE	= 3,
-			FOUR	= 4,
-			FIVE	= 5,
-			SIX		= 6,
-			SEVEN	= 7,
-			EIGHT	= 8,
-			NINE	= 9,
-			TEN		= 10,
-			J		= 11,
-			Q		= 12,
-			K		= 13,
-			A		= 14
-		};
+        public enum Suit
+        {
+            SPADES,     //0
+            CLUBS,      //1
+            DIAMONDS,   //2
+            HEARTS      //3
+        };
 
-		public Type cardNum;
-		
-		public Suit suit;
+        public enum Type
+        {
+            TWO = 2,
+            THREE = 3,
+            FOUR = 4,
+            FIVE = 5,
+            SIX = 6,
+            SEVEN = 7,
+            EIGHT = 8,
+            NINE = 9,
+            TEN = 10,
+            J = 11,
+            Q = 12,
+            K = 13,
+            A = 14
+        };
 
-		public Card(Type cardNum, Suit suit){
-			this.cardNum = cardNum;
-			this.suit = suit;
-		}
+        public Type cardNum;
 
-		public override string ToString(){
-			return "The " + cardNum + " of " + suit;
-		}
+        public Suit suit;
 
-		public int GetCardHighValue(){
-			int val;
+        public Card(Type cardNum, Suit suit)
+        {
+            this.cardNum = cardNum;
+            this.suit = suit;
+        }
 
-			switch(cardNum){
-			case Type.A:
-				val = 11; // the card value should be 1, when the handValue is >= 10
-				break;
-			case Type.K:
-			case Type.Q:
-			case Type.J:
-				val = 10;
-				break;	
-			default:
-				val = (int)cardNum;
-				break;
-			}
+        public override string ToString()
+        {
+            return "The " + cardNum + " of " + suit;
+        }
 
-			return val;
-		}
-	}
+        public int GetCardHighValue()
+        {
+            int val;
 
-	public static ShuffleBag<Card> deck;
+            switch (cardNum)
+            {
+                case Type.A:
+                    val = 11; // the card value should be 1, when the handValue is >= 10
+                    break;
+                case Type.K:
+                case Type.Q:
+                case Type.J:
+                    val = 10;
+                    break;
+                default:
+                    val = (int)cardNum;
+                    break;
+            }
 
-	// Use this for initialization
-	void Awake () {
+            return val;
+        }
+    }
 
-		if(!IsValidDeck()){
-			deck = new ShuffleBag<Card>();
+    public static ShuffleBag<Card> deck;
 
-			AddCardsToDeck();
-		}
+    // Use this for initialization
+    void Awake()
+    {
 
-		Debug.Log("Cards in Deck: " + deck.Count);
-	}
+        if (!IsValidDeck())
+        {
+            deck = new ShuffleBag<Card>();
 
-	protected virtual bool IsValidDeck(){
-		return deck != null; 
-	}
+            AddCardsToDeck();
+        }
 
-	protected virtual void AddCardsToDeck(){
-		foreach (Card.Suit suit in Card.Suit.GetValues(typeof(Card.Suit))){
-			foreach (Card.Type type in Card.Type.GetValues(typeof(Card.Type))){
-				deck.Add(new Card(type, suit));
-			}
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+        Debug.Log("Cards in Deck: " + deck.Count);
+    }
 
-	public virtual Card DrawCard(){
-		Card nextCard = deck.Next();
+    protected virtual bool IsValidDeck()
+    {
+        return deck != null;
+    }
 
-		return nextCard;
-	}
+    protected virtual void AddCardsToDeck()
+    {
+        foreach (Card.Suit suit in Card.Suit.GetValues(typeof(Card.Suit)))
+        {
+            foreach (Card.Type type in Card.Type.GetValues(typeof(Card.Type)))
+            {
+                deck.Add(new Card(type, suit));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public virtual Card DrawCard()
+    {
+        Card nextCard = deck.Next();
+
+        return nextCard;
+    }
 
 
-	public string GetNumberString(Card card){
-		if(card.cardNum.GetHashCode() <= 10){
-			return card.cardNum.GetHashCode() + "";
-		} else {
-			return card.cardNum + "";
-		}
-	}
-		
-	public Sprite GetSuitSprite(Card card){
-		return cardSuits[card.suit.GetHashCode()];
-	}
+    public string GetNumberString(Card card)
+    {
+        if (card.cardNum.GetHashCode() <= 10)
+        {
+            return card.cardNum.GetHashCode() + "";
+        }
+        else
+        {
+            return card.cardNum + "";
+        }
+    }
+
+    public Sprite GetSuitSprite(Card card)
+    {
+        return cardSuits[card.suit.GetHashCode()];
+    }
 }
