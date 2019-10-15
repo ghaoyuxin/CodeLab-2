57f110d085f190ff0a3e6f912b659718215df7a6
ghaoyuxin <ghaoyuxin@gmail.com>
2019-10-15 00:47:51 -0400

    make random grid map on start

--- W6_Gao/AStar/Assets/Scripts/Grids/FirstGridScript.cs
+++ W6_Gao/AStar/Assets/Scripts/Grids/FirstGridScript.cs
@@ -2,62 +2,68 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class FirstGridScript : GridScript {
+public class FirstGridScript : GridScript
+{
 
-	string[] gridString = new string[]{
-		"ww--|-rw-|----|------------",
+    string[] gridString = new string[]{
+        "ww--|-rw-|----|------------", // 27 by 15
 		"-ww-|-wr-rrrr-|---rrrr-----",
-		"-ww-|----r----|------r--www",
-		"-ww-|----r----|------r--ww-",
-		"-ww-|----rrrrrrrrrrrr--dww-",
-		"-wwwwwwww|----|-------dww--",
-		"-wwwwwwwwww---|------d-----",
-		"----|---www---|-----dd-----",
-		"----|---www---|----dd------",
-		"--ddd----w--www---dd-------",
-		"--drd----wwww-w--dd--------",
-		"--drd----|----wwdd---------",
-		"--ddd----|----|wdd---------",
-		"----dddddd----|------------",
-		"----|----d----|------------",
-	};
+        "-ww-|----r----|------r--www",
+        "-ww-|----r----|------r--ww-",
+        "-ww-|----rrrrrrrrrrrr--dww-",
+        "-wwwwwwww|----|-------dww--",
+        "-wwwwwwwwww---|------d-----",
+        "----|---www---|-----dd-----",
+        "----|---www---|----dd------",
+        "--ddd----w--www---dd-------",
+        "--drd----wwww-w--dd--------",
+        "--drd----|----wwdd---------",
+        "--ddd----|----|wdd---------",
+        "----dddddd----|------------",
+        "----|----d----|------------",
+    };
 
-	// Use this for initialization
-	void Start () {
-		gridWidth = gridString[0].Length;
-		gridHeight = gridString.Length;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        gridWidth = gridString[0].Length;
+        gridHeight = gridString.Length;
+    }
 
-	public override float GetMovementCost(GameObject go){
-		return base.GetMovementCost(go);
-	}
-	
-	protected override Material GetMaterial(int x, int y){
+    // Update is called once per frame
+    void Update()
+    {
 
-		var c = gridString[y].ToCharArray()[x];
+    }
 
-		Material mat;
+    public override float GetMovementCost(GameObject go)
+    {
+        return base.GetMovementCost(go);
+    }
 
-		switch(c){
-			case 'd': 
-				mat = mats[1];
-				break;
-			case 'w': 
-				mat = mats[2];
-				break;
-			case 'r': 
-				mat = mats[3];
-				break;
-			default: 
-				mat = mats[0];
-				break;
-		}
-	
-		return mat;
-	}
+    protected override Material GetMaterial(int x, int y)
+    {
+
+        var c = gridString[y].ToCharArray()[x];
+
+        Material mat;
+
+        switch (c)
+        {
+            case 'd':
+                mat = mats[1];
+                break;
+            case 'w':
+                mat = mats[2];
+                break;
+            case 'r':
+                mat = mats[3];
+                break;
+            default:
+                mat = mats[0];
+                break;
+        }
+
+        return mat;
+    }
 }
--- W6_Gao/AStar/Assets/Scripts/Grids/RandomGridScript.cs
+++ W6_Gao/AStar/Assets/Scripts/Grids/RandomGridScript.cs
@@ -6,39 +6,64 @@ using System.Collections.Generic;
 public class RandomGridScript : GridScript
 {
 
-	public static readonly float rockPercentage = 0.2f; 		// 20% chance of rocks
-	public static readonly float forestPercentage = 0.05f;		// 5% chance of forest
-	public static readonly float waterPercentage = 0.1f;		// 10% chance of water
+    public static readonly float rockPercentage = 0.2f;         // 20% chance of rocks
+    public static readonly float forestPercentage = 0.05f;      // 5% chance of forest
+    public static readonly float waterPercentage = 0.1f;        // 10% chance of water
 
-	string[] gridString;
+    string[] gridString;
 
-	private void Awake()
-	{
-		
-		// Make the grid be generated into gridString at random w/ the above percentages.
-	}
+    private void Awake()
+    {
+        //make a for loop that creates a string/line
+        MakeGrid(rockPercentage, forestPercentage, waterPercentage);
 
-	protected override Material GetMaterial(int x, int y){
+        // Make the grid be generated into gridString at random w/ the above percentages.
+    }
 
-		var c = gridString[y].ToCharArray()[x];
+    public void MakeGrid(float rockPercentage, float forestPercentage, float waterPercentage)
+    {
+        gridString = new string[gridHeight];
 
-		Material mat;
+        for (int i = 0; i < gridHeight; i++)
+        {
+            var lineOfGrid = "";
+            for (int j = 0; j < gridWidth; j++)
+            {
+                var mapTile = UnityEngine.Random.Range(0, 100);
+                if (mapTile < rockPercentage * 100) lineOfGrid += "r";//make a line of the arrays
+                else if (mapTile < rockPercentage * 100 + forestPercentage * 100) lineOfGrid += "f";
+                else if (mapTile < (rockPercentage + forestPercentage + waterPercentage) * 100) lineOfGrid += "w";
+                else lineOfGrid += "-";
+            }
+            gridString[i] = lineOfGrid;
+            //print(lineOfGrid);
 
-		switch(c){
-			case 'd': 
-				mat = mats[1];
-				break;
-			case 'w': 
-				mat = mats[2];
-				break;
-			case 'r': 
-				mat = mats[3];
-				break;
-			default: 
-				mat = mats[0];
-				break;
-		}
-	
-		return mat;
-	}
+        }
+    }
+
+    protected override Material GetMaterial(int x, int y)
+    {
+
+        var c = gridString[y].ToCharArray()[x];
+
+        Material mat;
+
+        switch (c)
+        {
+            case 'r':
+                mat = mats[1];
+                break;
+            case 'f':
+                mat = mats[2];
+                break;
+            case 'w':
+                mat = mats[3];
+                break;
+            default:
+                mat = mats[0];
+                break;
+        }
+
+        return mat;
+    }
 }
