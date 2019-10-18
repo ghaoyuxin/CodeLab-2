a04c3fd2804aec8bc371adb13e131387b200e1ae
ghaoyuxin <ghaoyuxin@gmail.com>
2019-10-18 13:03:12 -0400

    added 2 heuristics

--- W6_Gao/AStar/Assets/Scripts/Astar/HueristicScript.cs
+++ W6_Gao/AStar/Assets/Scripts/Astar/HueristicScript.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HueristicScript : MonoBehaviour {
-		
-	public virtual float Hueristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript){
-		return Random.Range(0, 500);
-	}
+public class HueristicScript : MonoBehaviour
+{
+
+    public virtual float Hueristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript)
+    {
+        //return 0; //heuristic 1
+        return Mathf.Abs(start.x - goal.x) + Mathf.Abs(start.y - goal.y); // heuristic 2
+
+    }
 }
