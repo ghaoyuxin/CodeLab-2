251b7e2d0dd50507325fc4b154a9ce798050b2ee
ghaoyuxin <ghaoyuxin@gmail.com>
2019-10-19 19:02:48 -0400

    Added Heuristic 3. The faster Heuristic is 1 and 2.

--- W6_Gao/AStar/Assets/Scripts/Astar/HueristicScript.cs
+++ W6_Gao/AStar/Assets/Scripts/Astar/HueristicScript.cs
@@ -6,8 +6,11 @@ public class HueristicScript : MonoBehaviour
 
     public virtual float Hueristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript)
     {
-        //return 0; //heuristic 1
-        return Mathf.Abs(start.x - goal.x) + Mathf.Abs(start.y - goal.y); // heuristic 2
+        //return 0; //heuristic 1 is the original A Star
+        //return Mathf.Abs(start.x - goal.x) + Mathf.Abs(start.y - goal.y); // heuristic 2 emphasize on the distance to the goal, why this doesn't change anything?
 
+        return Random.Range(0, 100);//Heuristic 3
+
+        //the faster heuristic for both 1stGridScene and 2ndGridScene is Heurstic 1 or Heuristic 2(they are the same result??)
     }
 }
--- W6_Gao/AStar/Assets/Scripts/Grids/GridScript.cs
+++ W6_Gao/AStar/Assets/Scripts/Grids/GridScript.cs
@@ -2,72 +2,80 @@
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class GridScript : MonoBehaviour {
+public class GridScript : MonoBehaviour
+{
 
-	public int gridWidth;
-	public int gridHeight;
-	public float spacing;
-	
-	public Material[] mats;
-	public float[]   costs;
+    public int gridWidth;
+    public int gridHeight;
+    public float spacing;
 
-	public Vector3 start = new Vector3(0,0);
-	public Vector3 goal = new Vector3(14,14);
-	
-	GameObject[,] gridArray;
-	
-	public GameObject startSprite;
-	public GameObject goalSprite;
+    public Material[] mats;
+    public float[] costs;
 
-	public GameObject[,] GetGrid(){
-		
-		if (gridArray != null) return gridArray;
-		
-		gridArray = new GameObject[gridWidth, gridHeight];
-			
-		var offsetX = (gridWidth  * -spacing)/2f;
-		var offsetY = (gridHeight * spacing)/2f;
+    public Vector3 start = new Vector3(0, 0);
+    public Vector3 goal = new Vector3(14, 14);
 
-		for(var x = 0; x < gridWidth; x++){
-			for(var y = 0; y < gridHeight; y++){
-				var quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-				quad.transform.localScale = new Vector3(spacing, spacing, spacing);
-				quad.transform.position = new Vector3(offsetX + x * spacing, 
-					offsetY - y * spacing, 0);
+    public GameObject[,] gridArray;
 
-				quad.transform.parent = transform;
+    public GameObject startSprite;
+    public GameObject goalSprite;
 
-				gridArray[x, y] = quad;
-					
-				quad.GetComponent<MeshRenderer>().sharedMaterial = GetMaterial(x, y);
+    public GameObject[,] GetGrid()
+    {
 
-				if(Math.Abs(goal.x - x) < 0.01f && Math.Abs(goal.y - y) < 0.01f){
-					goalSprite.transform.position = quad.transform.position;
-				}
-				if(Math.Abs(start.x - x) < 0.01f && Math.Abs(start.y - y) < 0.01f){
-					startSprite.transform.position = quad.transform.position;
-				}
-			}
-		}
+        if (gridArray != null) return gridArray;
 
-		return gridArray;
-	}
+        gridArray = new GameObject[gridWidth, gridHeight];
 
-	public virtual float GetMovementCost(GameObject go){
-		var mat = go.GetComponent<MeshRenderer>().sharedMaterial;
-		int i;
+        var offsetX = (gridWidth * -spacing) / 2f;
+        var offsetY = (gridHeight * spacing) / 2f;
 
-		for(i = 0; i < mats.Length; i++){
-			if(mat.name.StartsWith(mats[i].name))
-			{
-				break;
-			}
-		}
-		return costs[i];
-	}
+        for (var x = 0; x < gridWidth; x++)
+        {
+            for (var y = 0; y < gridHeight; y++)
+            {
+                var quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                quad.transform.localScale = new Vector3(spacing, spacing, spacing);
+                quad.transform.position = new Vector3(offsetX + x * spacing,
+                    offsetY - y * spacing, 0);
 
-	protected virtual Material GetMaterial(int x, int y)
-	{
-		return mats[Random.Range(0,mats.Length)];
-	}
+                quad.transform.parent = transform;
+
+                gridArray[x, y] = quad;
+
+                quad.GetComponent<MeshRenderer>().sharedMaterial = GetMaterial(x, y);
+
+                if (Math.Abs(goal.x - x) < 0.01f && Math.Abs(goal.y - y) < 0.01f)
+                {
+                    goalSprite.transform.position = quad.transform.position;
+                }
+                if (Math.Abs(start.x - x) < 0.01f && Math.Abs(start.y - y) < 0.01f)
+                {
+                    startSprite.transform.position = quad.transform.position;
+                }
+            }
+        }
+
+        return gridArray;
+    }
+
+    public virtual float GetMovementCost(GameObject go)
+    {
+        var mat = go.GetComponent<MeshRenderer>().sharedMaterial;
+        int i;
+
+        for (i = 0; i < mats.Length; i++)
+        {
+            if (mat.name.StartsWith(mats[i].name))
+            {
+                break;
+            }
+        }
+        return costs[i];
+    }
+
+    protected virtual Material GetMaterial(int x, int y)
+    {
+        return mats[Random.Range(0, mats.Length)];
+    }
 }
