5977d37112e7f53ef5f1614284a3b0c623a870c7
ghaoyuxin <ghaoyuxin@gmail.com>
2019-10-25 05:26:27 -0400

    added enum and struct to model

--- W7_Gao/TicTacToe/Assets/Scripts/Model.cs
+++ W7_Gao/TicTacToe/Assets/Scripts/Model.cs
@@ -2,7 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Model : MonoBehaviour
+public class Model
 {
+    public enum Players
+    {
+        X,
+        O,
+    };
+
+    private struct Piece
+    {
+        public bool hasMoved;
+        public int moveX;
+        public int moveY;
+
+    }
+
+    Piece[,] board = new Piece[3, 3];
 
 }
