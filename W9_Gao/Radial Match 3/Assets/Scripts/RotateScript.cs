2b4d5c518ff9eb1ed22cfbdc87b0b1ad2ed42452
ghaoyuxin <ghaoyuxin@gmail.com>
2019-11-07 17:49:24 -0500

    added rotate script

---
+++ W9_Gao/Radial Match 3/Assets/Scripts/RotateScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    public int rotateAngle = 0;
+    public int rotateDirection = 0;
+    public Transform rotateRing;
+
+    private void OnMouseDown()
+    {
+        rotateRing.transform.Rotate(0, 0, rotateAngle * rotateDirection, Space.Self);
+    }
+}
