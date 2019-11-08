775c4b26d978522b43667dc7caab404dd8c65535
ghaoyuxin <ghaoyuxin@gmail.com>
2019-11-07 19:03:27 -0500

    added generated dots script, the offset angle doesn't quite work, tweaking

---
+++ W9_Gao/Radial Match 3/Assets/Scripts/GenerateScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateScript : MonoBehaviour
+{
+    public float radius = 0f;
+    public int numberOfDots = 0;
+    public float angleOffset = 0;
+
+    public GameObject blue, green, orange, purple;
+
+    private void Start()
+    {
+        for (int i = 0; i < numberOfDots; i++)
+        {
+            float angle = angleOffset + i * Mathf.PI * 2f / numberOfDots;
+            Vector3 newPos = new Vector3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);
+            GameObject ring = Instantiate(blue, newPos, Quaternion.identity);
+        }
+
+    }
+}
