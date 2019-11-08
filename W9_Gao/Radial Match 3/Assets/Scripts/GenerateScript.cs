1df006865035ad3ba129e3d251a567b1aaa97279
ghaoyuxin <ghaoyuxin@gmail.com>
2019-11-07 19:59:41 -0500

    randomized dots

--- W9_Gao/Radial Match 3/Assets/Scripts/GenerateScript.cs
+++ W9_Gao/Radial Match 3/Assets/Scripts/GenerateScript.cs
@@ -11,15 +11,11 @@ public class GenerateScript : MonoBehaviour
     public Transform innerRing;
     public Transform middleRing;
     public Transform outerRing;
-    private GameObject _blue, _green, _orange, _purple;
+    private GameObject[] dotList;
 
     private void Start()
     {
-        _blue = GameObject.FindWithTag("blue");
-        _green = GameObject.FindWithTag("green");
-        _orange = GameObject.FindWithTag("orange");
-        _purple = GameObject.FindWithTag("purple");
-
+        dotList = Resources.LoadAll<GameObject>("");
         GenerateDots(0.55f, 3, 60, innerRing);//inner ring
         GenerateDots(1.5f, 6, 0, middleRing);//middle ring
         GenerateDots(2.4f, 12, 0, outerRing);//outer ring
@@ -29,9 +25,10 @@ public class GenerateScript : MonoBehaviour
     {
         for (int i = 0; i < numberOfDots; i++)
         {
+            GameObject dotToGenerate = dotList[Random.Range(0, dotList.Length)];
             float angle = angleOffset * Mathf.Deg2Rad + i * Mathf.PI * 2f / numberOfDots;
             Vector3 newPos = new Vector3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);
-            GameObject dot = Instantiate(Resources.Load<GameObject>("blue"), newPos, Quaternion.identity);
+            GameObject dot = Instantiate(dotToGenerate, newPos, Quaternion.identity);
             dot.transform.SetParent(parent);
         }
 
