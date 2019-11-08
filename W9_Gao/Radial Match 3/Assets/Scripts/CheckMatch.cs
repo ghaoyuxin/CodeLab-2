578e7942f6bcd55b60fb9383b49e2603fd26a764
ghaoyuxin <ghaoyuxin@gmail.com>
2019-11-07 21:49:52 -0500

    added check match script, not working yet

---
+++ W9_Gao/Radial Match 3/Assets/Scripts/CheckMatch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckMatch : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print("on trigger enter");
+        if (other.gameObject.CompareTag("blue"))
+        {
+            print("found");
+            Destroy(other.gameObject);
+        }
+    }
+}
