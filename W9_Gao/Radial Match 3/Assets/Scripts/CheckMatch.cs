7c3f2d88b13d0a1229ac23f98cdc18e3ee539731
ghaoyuxin <ghaoyuxin@gmail.com>
2019-11-07 22:39:20 -0500

    unfortunately this way of checking match doesn't work. I am about to comment out this chulk of code.

--- W9_Gao/Radial Match 3/Assets/Scripts/CheckMatch.cs
+++ W9_Gao/Radial Match 3/Assets/Scripts/CheckMatch.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 
 public class CheckMatch : MonoBehaviour
 {
+    private int _blueTag = 0, _greenTag = 0, _orangeTag = 0, _purpleTag = 0;
+    private List<Collider2D> TriggerList = new List<Collider2D>();
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        print("on trigger enter");
-        if (other.gameObject.CompareTag("blue"))
+        if (!TriggerList.Contains(other))
         {
-            print("found");
-            Destroy(other.gameObject);
+            //add the object to the list
+            TriggerList.Add(other);
+        }
+
+        if (TriggerList.Count == 3)
+        {
+            if (TriggerList[0] == TriggerList[1] && TriggerList[0] == TriggerList[2]) Destroy(other.gameObject);
+            else return;
+        }
+
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        //if the object is in the list
+        if (TriggerList.Contains(other))
+        {
+            //remove it from the list
+            TriggerList.Remove(other);
+            print(TriggerList);
         }
     }
+
+    // if (other.gameObject.CompareTag("blue"))
+    //     print("on trigger enter");
+    // if (other.gameObject.CompareTag("blue"))
+    // {
+    //     print("found");
+    //     Destroy(other.gameObject);
+    // }
+
+
 }
