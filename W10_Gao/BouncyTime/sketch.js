cf50a2ad27b4ed0db6c0e034a51feb39ceca9585
ghaoyuxin <ghaoyuxin@gmail.com>
2019-11-12 23:05:22 -0500

    added p5 play library to the project, tested it works by drawing a sprite

--- W10_Gao/BouncyTime/sketch.js
+++ W10_Gao/BouncyTime/sketch.js
@@ -1,7 +1,9 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
 }
 
 function draw() {
-
+  background(255,255,255);
+  drawSprites();
 }
